3fb0b4e823ad32f61ff2b12cbf061b5a4c59afdb
restartwithpython <tusharmandali321@gmail.com>
2025-06-26 02:08:38 +0530

    ✅ Day 3 - Defined Operators & Expressions | Phase 2 Started

---
+++ daywise-logs/day-03 (Operators)/04_Comparision & Logical.py
@@ -0,0 +1,21 @@
+# Comparison Operator is used for Comparing two Values. The Output of it is always (Yes or No) OR (True or False)
+
+a = 10
+b = 20
+print(a < b)
+
+print(a == b)  # Equal to
+print(a != b)  # Is not Equal to
+
+
+# Logical Operator :
+"""
+Not Operator reverse the result output
+"""
+
+physics = 78
+chemistry = 18
+
+print(physics > 33 and chemistry > 33)
+print(physics > 33 or chemistry > 33)
+print(not (physics > 33 and chemistry > 33))
---
+++ daywise-logs/day-03 (Operators)/05_Bitwise.py
@@ -0,0 +1,29 @@
+"""
+Bitwise Operator :
+
+& performs Binary AND
+| performs Binary OR
+^ Performs Binary XOR Operator
+~ Performs a Binary NOT Operator
+<< Shift Left Operation
+>> Shift Right Operation
+
+IS & IN Operator
+is  Identity Operator it use to compare two object and check if both are same or not and return True or False Value based on result
+in  Membership Operator This is used to check if the mentioned value is present in the List or Not
+
+
+Ternary Operators
+ Code without Ternary Operator
+def is_adult(age):
+    if age < 18:
+        return True
+    else:
+        return False
+
+
+Code with Ternary Operator
+def is_adult1(age):
+    return True if age < 18 else False
+
+"""
