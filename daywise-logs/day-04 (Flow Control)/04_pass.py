eae0d8214c0c4bc024c6661c5261a5b1389aadf4
restartwithpython <tusharmandali321@gmail.com>
2025-06-27 23:07:55 +0530

    🧠 Day 4: Learned if-else, nested conditions, and logical expressions

---
+++ daywise-logs/day-04 (Flow Control)/04_pass.py
@@ -0,0 +1,13 @@
+"""
+pass is a placeholder statement.
+if condition:
+    pass  # nothing happens, no error
+
+"""
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    pass  # logic will be added later
+else:
+    print("Minor")
